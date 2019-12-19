b4f0d3926a79e0a43640247c0cf9397f77b24f61
ddai <ddai@tripadvisor.com>
2019-12-19 17:02:55 +1100

    fixing local date serialisation/deserialisation wip

--- src/main/java/com/micro/services/event/bus/event/model/ProductAvailablePeriod.java
+++ src/main/java/com/micro/services/event/bus/event/model/ProductAvailablePeriod.java
@@ -4,8 +4,14 @@ import java.time.LocalDate;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateSerializer;
 
 public class ProductAvailablePeriod {
+    @JsonDeserialize(using = LocalDateDeserializer.class)
+    @JsonSerialize(using = LocalDateSerializer.class)
     private LocalDate from;
     private LocalDate to;
 
